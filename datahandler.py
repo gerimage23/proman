7e654018a364b1b49369f9cbe4613a95863f3636
SzabolcsHornyak <design85@gmail.com>
2017-07-04 11:16:32 +0200

    Add: Json PSql connect, USER endpoint

--- datahandler.py
+++ datahandler.py
@@ -1,32 +1,44 @@
+import json
 import psycopg2
-from werkzeug import security
-import os
-import urllib
-
-CONNECTION_STRING = "dbname=url.path[1:] user=url.username host=url.hostname password=url.password"
 
 
-def init_db_connection(connection_string=CONNECTION_STRING):
+def execute_sql_statement(sql_statement, values=tuple()):
+    # setup connection string, not the most secure way
+ 
+    with open('static/connection.json') as data_file:   
+        data = json.load(data_file)
 
+    dbname = data["connection"]["database"]
+    user = data["connection"]["user"]
+    host = data["connection"]["host"]
+    password = data["connection"]["password"]
+    connect_str = "dbname="+dbname+" user="+user+" host="+host+" password="+password
+    conn = None
     try:
-
-        urllib.parse.uses_netloc.append('postgres')
-        url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
-        
-        connection = psycopg2.connect(
-            database=url.path[1:],
-            user=url.username,
-            password=url.password,
-            host=url.hostname,
-            port=url.port
-        )
-
-        connection.autocommit = True
-
-        cursor = connection.cursor()
-
-    except psycopg2.DatabaseError as e:
+        conn = psycopg2.connect(connect_str)
+    except psycopg2.DatabaseError as e:  # TODO don't use this, remember: "raise PythonicError("Errors should never go silently.")
         print(e)
         return [[e]]
+    else:
+        conn.autocommit = True
+        cursor = conn.cursor()
+        try:
+            cursor.execute(sql_statement, values)
+        except psycopg2.ProgrammingError as e:
+            print(e)
+            return [[e]]
+        else:
+            if sql_statement.split(' ')[0].lower() == 'select':
+                rows = list(cursor.fetchall())
+                return rows
+    finally:
+        if conn:
+            # conn.commit() leaving it here for future testing to see how it works
+            conn.close()
 
-    return cursor
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
