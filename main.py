d8bf3a0327bba23e665c2049a1e30479c035f7a6
akosENagy <akos.e.nagy@gmail.com>
2017-07-04 02:11:47 +0200

    Added register_user(), render_form_page() and necessary imports from flask and datahandler.

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
+import datahandler
+
+
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+
+@app.route("/register")
+def render_form_page():
+    return render_template('form.html')
+
+
+@app.route("/register-user", methods=["POST"])
+def register_user():
+    username = request.form["username"]
+    password = request.form["password"]
+    if datahandler.insert_user(username, password):
+        session['username'] = username
+        return redirect(url_for('hello'))
+    else:
+        return render_template('form.html', errormsg="Username already exists!")
 
 
 @app.route("/")
@@ -8,7 +28,7 @@ def hello():
 
 
 def main():
-    app.run()
+    app.run(debug=True)
 
 if __name__ == '__main__':
     main()
