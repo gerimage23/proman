801ab710e636d185cdb30d84193924c28d6785fb
akosENagy <akos.e.nagy@gmail.com>
2017-07-04 02:10:50 +0200

    Added insert_user() to datahandler.py

--- datahandler.py
+++ datahandler.py
@@ -3,7 +3,7 @@ from werkzeug import security
 import os
 import urllib
 
-CONNECTION_STRING = "dbname=url.path[1:] user=url.username host=url.hostname password=url.password"
+CONNECTION_STRING = "dbname=proman_testdb user=aakeeka host=localhost password=postgresql"
 
 
 def init_db_connection(connection_string=CONNECTION_STRING):
@@ -12,7 +12,7 @@ def init_db_connection(connection_string=CONNECTION_STRING):
 
         urllib.parse.uses_netloc.append('postgres')
         url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
-        
+
         connection = psycopg2.connect(
             database=url.path[1:],
             user=url.username,
@@ -30,3 +30,18 @@ def init_db_connection(connection_string=CONNECTION_STRING):
         return [[e]]
 
     return cursor
+
+
+def insert_user(username, password):
+    '''
+    Inserts a username-password pair into the database after hashing the password.
+    '''
+    password = security.generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+
+    cursor = init_db_connection()
+    try:
+        cursor.execute('''INSERT INTO users (username, password) VALUES (%s,%s) ;''', (username, password))
+        return True
+    except psycopg2.DatabaseError as e:
+        print(e)
+        return False
