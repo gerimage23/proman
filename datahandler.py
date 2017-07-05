196dc90ffe5930d5dc0cf2894eba971a7a5ec690
GergoSzabo23 <gergo.szabo23@gmail.com>
2017-07-05 13:03:05 +0200

    Revert "Akos user registration"

--- datahandler.py
+++ datahandler.py
@@ -1,12 +1,5 @@
 import json
 import psycopg2
-from werkzeug import security
-import os
-import urllib
-from datetime import datetime
-
-CONNECTION_STRING = "dbname=proman_testdb user=aakeeka host=localhost password=postgresql"
-CONNECTION_URL = "postgres://sbcjqryaikuvtq:64ea9d49945ec0b2ba2838400f964055dc138b9334ad3117b258914dc11d6b7b@ec2-54-75-229-201.eu-west-1.compute.amazonaws.com:5432/d1vmbir57f49qf"
 
 
 def execute_sql_statement(sql_statement, values=tuple()):
@@ -22,67 +15,6 @@ def execute_sql_statement(sql_statement, values=tuple()):
     connect_str = "dbname="+dbname+" user="+user+" host="+host+" password="+password
     conn = None
     try:
-
-        urllib.parse.uses_netloc.append('postgres')
-        url = urllib.parse.urlparse(CONNECTION_URL)  # os.environ.get('DATABASE_URL') IF ON HEROKU
-
-        connection = psycopg2.connect(
-            database=url.path[1:],
-            user=url.username,
-            password=url.password,
-            host=url.hostname,
-            port=url.port
-        )
-
-        connection.autocommit = True
-
-        cursor = connection.cursor()
-
-    except psycopg2.DatabaseError as e:
-        print(e)
-        return [[e]]
-
-    return cursor
-
-
-def insert_user(username, password):
-    '''
-    Inserts a username-password pair into the database after hashing the password.
-    '''
-    password = security.generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
-    lastlog_time = datetime.now().replace(microsecond=0)
-
-    cursor = init_db_connection()
-    try:
-        cursor.execute('''INSERT INTO users (username, password, lastlog_time)
-                          VALUES (%s,%s, %s);''', (username, password, lastlog_time))
-        return True
-    except psycopg2.DatabaseError as e:
-        print(e)
-        return False
-
-
-def check_user(username, password):
-        try:
-            cursor = init_db_connection(connection_string=CONNECTION_URL)
-            cursor.execute("""SELECT username, password
-                            FROM users
-                            WHERE username=%s;""", (username,))
-
-            try:
-                results = cursor.fetchall()[0]
-
-            except IndexError as e:
-                print(e)
-                return False
-
-            if username == username and security.check_password_hash(results[1], password):
-                return True
-
-        except Exception as e:
-            print(e)
-            return [[e]]
-
         conn = psycopg2.connect(connect_str)
     except psycopg2.DatabaseError as e:  # TODO don't use this, remember: "raise PythonicError("Errors should never go silently.")
         print(e)
@@ -110,4 +42,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
